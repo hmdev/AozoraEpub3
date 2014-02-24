0dbf7474b1f8c9d5d1f7141043e876ec5f4cc219
hmdev <hmdev@PC>
2014-02-25 00:01:28 +0900

    小説家になろうの構成変更に対応 n行削除のプロファイル読み込み修正

--- src/AozoraEpub3Applet.java
+++ src/AozoraEpub3Applet.java
@@ -3542,7 +3542,7 @@ public class AozoraEpub3Applet extends JApplet
 		setPropsSelected(jCheckCommentPrint, props, "CommentPrint");
 		setPropsSelected(jCheckCommentConvert, props, "CommentConvert");
 		//空行除去
-		try { jComboxRemoveEmptyLine.setSelectedItem(Integer.parseInt(props.getProperty("RemoveEmptyLine"))); } catch (Exception e) {}
+		try { jComboxRemoveEmptyLine.setSelectedIndex(Integer.parseInt(props.getProperty("RemoveEmptyLine"))); } catch (Exception e) {}
 		propValue = props.getProperty("MaxEmptyLine");
 		try { jComboxMaxEmptyLine.setSelectedIndex(Integer.parseInt(propValue)); } catch (Exception e) {}
 		//行頭字下げ追加
