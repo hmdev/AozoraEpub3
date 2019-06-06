09d49b4c265cd930b6c43189c9663fbea117ce5d
kyukyunyorituryo <kyukyunyorituryo@live.jp>
2019-06-06 22:50:21 +0900

    ビルドのための変更

--- src/AozoraEpub3.java
+++ src/AozoraEpub3.java
@@ -31,7 +31,7 @@ import com.github.junrar.rarfile.FileHeader;
 /** コマンドライン実行用mainとePub3変換関数 */
 public class AozoraEpub3
 {
-	public static final String VERSION = "1.1.0b49Q";
+	public static final String VERSION = "1.1.0b50Q";
 
 	/** コマンドライン実行用 */
 	public static void main(String args[])
