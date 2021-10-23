dde954e3a97beddcef57679a1135c864b0400258
kyukyunyorituryo <kyukyunyorituryo@live.jp>
2021-10-23 23:21:15 +0900

    1.1.1b10Qビルド

--- src/AozoraEpub3.java
+++ src/AozoraEpub3.java
@@ -32,7 +32,7 @@ import com.github.junrar.rarfile.FileHeader;
 /** コマンドライン実行用mainとePub3変換関数 */
 public class AozoraEpub3
 {
-	public static final String VERSION = "1.1.1b9Q";
+	public static final String VERSION = "1.1.1b10Q";
 
 	/** コマンドライン実行用 */
 	public static void main(String args[])
